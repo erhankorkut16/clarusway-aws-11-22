462f0750e8f1ea01b1aa8e142ac3f88bdeb06a84
serdarcw <serkangumus06@gmail.com>
2022-05-24 22:39:41 -0500

    new

--- linux/projects/Project-101/First_Part/script.sh
+++ linux/projects/Project-101/First_Part/script.sh
@@ -1,1 +1,1 @@
-cat $FILE | grep serdar | grep Terminate | grep -Eo "i-[a-zA-Z0-9]{17}" > /tmp/result.txt
+cat event_history.csv | grep serdar | grep -i Terminateinstance | grep -Eo "i-[a-zA-Z0-9]{17}" > /tmp/result.txt
